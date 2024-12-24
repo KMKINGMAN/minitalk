a724e699cfef0361a103b76a7762bfd2c37da8f1
Muhammad Kurkar <mkurkar@c2r6s7.42amman.com>
2024-12-24 15:31:30 +0300

    chore: update .gitignore and refactor minitalk.h for improved structure and readability

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -13,43 +13,42 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <signal.h>
-# include <unistd.h>
-# include <stdlib.h>
-#include <stdio.h>
 # include "../libft/libft.h"
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000000
 # endif
 
-#if BUFFER_SIZE < 1
-# undef BUFFER_SIZE
-# define BUFFER_SIZE 1000000
-#endif
+# if BUFFER_SIZE < 1
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
 
 typedef enum e_bool
 {
 	FALSE,
 	TRUE
-} t_bool;
+}						t_bool;
 
 typedef struct s_message
 {
-	char	*message;
-	size_t		index;
-	int 	pid;
-	size_t		buffer_size;
+	char				*message;
+	size_t				index;
+	int					pid;
+	size_t				buffer_size;
 	struct s_message	*next;
 	unsigned char		buff;
-    int             bit_count;
-    int             active;
-}	t_message;
+	int					bit_count;
+	int					active;
+}						t_message;
 
-
-void		ft_args_checker(int argc, char **argv);
-t_message	*init(int pid);
-t_message	*search_or_create(t_message **head, int pid);
-void		*remove_message(t_message **head, int pid);
+void					ft_args_checker(int argc, char **argv);
+t_message				*init(int pid);
+t_message				*search_or_create(t_message **head, int pid);
+void					*remove_message(t_message **head, int pid);
 
 #endif
